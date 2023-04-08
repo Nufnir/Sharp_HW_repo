ab3d36fc878736321fc6dcdbd412dfdfb77dcb95
mikras <krasnov-mg@outlook.com>
2023-04-08 22:55:13 +0300

    Task 002. First seminar. Get 2 numbers from user, determine min and max then print min and max to console

---
+++ FirstSem_080423/Task002_minAndMax/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine();
+Console.Write("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+Console.Write("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine()!);
+int resultmax = numberA;
+int resultmin = numberB;
+
+if (numberA < numberB)
+{
+    resultmax = numberB;
+    resultmin = numberA;
+}
+
+Console.WriteLine();
+Console.WriteLine("Максимальное число из " + numberA + " и " + numberB + " равно " + resultmax + ". " + "Минимальное число равно " + resultmin + ".");
+Console.WriteLine();
