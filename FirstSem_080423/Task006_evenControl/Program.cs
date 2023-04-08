ba5b7f942260af41da3e805079bc2c92e2091611
mikras <krasnov-mg@outlook.com>
2023-04-08 23:42:55 +0300

    Task 006. First seminar. Get a number from user, determine if even, print the result to console

---
+++ FirstSem_080423/Task006_evenControl/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine();
+Console.Write("Введите число: ");
+int numberA = int.Parse(Console.ReadLine()!);
+
+if (numberA % 2 == 0)
+{
+    Console.WriteLine();
+    Console.WriteLine("Введённое число " + numberA + " чётное" + ".");
+    Console.WriteLine();
+}
+else
+{
+    Console.WriteLine();
+    Console.WriteLine("Введённое число " + numberA + " нечётное" + ".");
+    Console.WriteLine();
+}
