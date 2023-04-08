4e55469738d59df83efa1a90a552152097b0f78e
mikras <krasnov-mg@outlook.com>
2023-04-08 23:16:11 +0300

    Task 004. First seminar. Get 3 numbers from user, determine max, print max to console

---
+++ FirstSem_080423/Task004_maxOutOf3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine();
+Console.Write("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+Console.Write("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+Console.Write("Введите третье число: ");
+int numberC = int.Parse(Console.ReadLine()!);
+int resultmax = numberA;
+
+if (numberA < numberB) resultmax = numberB;
+
+if (resultmax < numberC) resultmax = numberC;
+
+Console.WriteLine();
+Console.WriteLine("Максимальное число из введённых " + numberA + " , " + numberB + " и " + numberC + " равно " + resultmax + ".");
+Console.WriteLine();
