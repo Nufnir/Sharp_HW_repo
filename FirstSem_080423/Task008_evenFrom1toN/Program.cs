6ccd2706bc1331521899990ace3feece21bd7603
mikras <krasnov-mg@outlook.com>
2023-04-08 23:45:09 +0300

    Task 008. First seminar. Get a number from user, print out all even numbers from 1 to users number

---
+++ FirstSem_080423/Task008_evenFrom1toN/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine();
+Console.Write("Введите число: ");
+int numberA = int.Parse(Console.ReadLine()!);
+int bufferNumber = 1;
+Console.WriteLine();
+Console.WriteLine("Чётные числа от " + bufferNumber + " до " + numberA + ":");
+
+while (bufferNumber <= numberA)
+{
+    if (bufferNumber % 2 == 0)
+    {
+        Console.Write(bufferNumber + " ");
+    }
+bufferNumber++;
+}
+Console.WriteLine();
