1e07a66ab060717af7f51629f738573ca1b84f93
mikras <krasnov-mg@outlook.com>
2023-04-09 22:53:27 +0300

    Second seminar. Task 010 get a three-digit number from user, print out second digit

---
+++ SecondSem_090423/Task010_secondDigitOfNum/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine();
+Console.Write("Введите трёхзначное число: ");
+int numberA = int.Parse(Console.ReadLine()!);
+
+if(numberA > 999 || numberA < 100) Console.WriteLine("Ввод неверный.");
+else
+{
+    Console.WriteLine("Введено число " + numberA);
+    int secondDigit = numberA / 10 % 10;
+    Console.WriteLine("Вторая цифра в числе: " + secondDigit);
+}
