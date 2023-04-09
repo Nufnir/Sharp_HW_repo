327a008894e91cdc2adabfaa20c6417a1a86108c
mikras <krasnov-mg@outlook.com>
2023-04-09 22:58:04 +0300

    Second seminar. Task 015. Get a week day number from user, tell if it is a weekend or a weekday

---
+++ SecondSem_090423/Task015_ifDayIsWeekend/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine();
+Console.Write("Введите номер дня недели: ");
+int numberA = int.Parse(Console.ReadLine()!);
+
+if(numberA < 1 || numberA > 7) Console.WriteLine("Такого дня в неделе нет.");
+else
+{
+    if(numberA >= 6) Console.WriteLine("Ура! Выходной!");
+    else Console.WriteLine("Серые будни :(");
+}
