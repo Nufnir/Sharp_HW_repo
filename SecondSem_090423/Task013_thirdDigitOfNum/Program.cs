113bbce769232c99692cb8bad3908c16c6335a07
mikras <krasnov-mg@outlook.com>
2023-04-09 22:55:36 +0300

    Second seminar. Task 013. Get a number from user, print out a third digit or tell if there is no third digit

---
+++ SecondSem_090423/Task013_thirdDigitOfNum/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine();
+Console.Write("Введите число: ");
+int numberA = int.Parse(Console.ReadLine()!);
+
+if(numberA < 100) Console.WriteLine("Третьей цифры в числе нет.");
+else
+{
+    int thirdDigit = numberA;
+    while(thirdDigit > 999)
+    {
+        thirdDigit /= 10;
+    }
+    thirdDigit %= 10;
+    Console.WriteLine("Третья цифра числа " + numberA + " это " + thirdDigit);
+}
