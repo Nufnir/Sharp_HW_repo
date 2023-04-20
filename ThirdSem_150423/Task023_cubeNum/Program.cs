513ab74eaa76700f84f49cb557274ba0f982f9de
mikras <krasnov-mg@outlook.com>
2023-04-20 12:02:46 +0300

    Third seminar. Task 023. Get a number from user make this number cubed

---
+++ ThirdSem_150423/Task023_cubeNum/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine();
+Console.Write("Введите число N для вывода кубов чисел от 1 до N: ");
+int numberA = int.Parse(Console.ReadLine()!);
+int indexNum = 1;
+int cubeNum;
+
+while (indexNum <= numberA)
+{
+    cubeNum = indexNum * indexNum * indexNum;
+    Console.WriteLine(indexNum + " в кубе равно " + cubeNum);
+    indexNum++;
+}
