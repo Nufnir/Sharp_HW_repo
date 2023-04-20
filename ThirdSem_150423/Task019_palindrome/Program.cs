5ead8b7290ec4c73d2cc5ce4e0e59bc9076f1937
mikras <krasnov-mg@outlook.com>
2023-04-20 11:58:11 +0300

    Third seminar. Task 019. Get a number from user, tell if this is a palindrome

---
+++ ThirdSem_150423/Task019_palindrome/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine();
+Console.Write("Введите число, будем проверять палиндром или нет: ");
+int numberA = int.Parse(Console.ReadLine()!);
+int temp = numberA;
+int numberB = 0;
+
+while(temp > 0)
+{
+    numberB = numberB * 10 + temp % 10;
+    temp /= 10;
+}
+
+if(numberA == numberB) Console.WriteLine("Число " + numberA + " палиндром!");
+else Console.WriteLine("Число " + numberA + " не палиндром.");
