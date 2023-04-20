7e49e6723d129cb7a7eb3d9e41ceb14606c52b8d
mikras <krasnov-mg@outlook.com>
2023-04-20 16:07:45 +0300

    Fourth seminar. Task 027. Get a number from user, calculate a sum of all digits in a number

---
+++ FourthSem_160423/Task027_sumOfNumInDigit/Program.cs
@@ -0,0 +1,18 @@
+﻿int SumDigits(int num)
+{
+    int result = 0;
+    while(num > 0)
+    {
+        result += num % 10;
+        num = num / 10;
+    }
+    return result;
+}
+
+Console.WriteLine();
+Console.Write("Введите число, а мы просуммируем цифры числа: ");
+int number = int.Parse(Console.ReadLine()!);
+int sumDigits = SumDigits(number);
+Console.WriteLine();
+Console.WriteLine($"Сумма чисел в числе {number} равна {sumDigits}");
+Console.WriteLine();
