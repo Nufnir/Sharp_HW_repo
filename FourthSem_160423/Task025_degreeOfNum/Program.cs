d9b6cf73199ddb31dc28b2ca3780019de8e76000
mikras <krasnov-mg@outlook.com>
2023-04-20 16:05:50 +0300

    Fourth seminar. Task 025. Get a number and an exponent from user, calculate an exponentation.

---
+++ FourthSem_160423/Task025_degreeOfNum/Program.cs
@@ -0,0 +1,16 @@
+﻿double DegreeOfNum(double arg1, double arg2)
+{
+    double result = Math.Pow(arg1, arg2);
+    return result;
+}
+
+Console.WriteLine();
+Console.Write("Введите число, которое нужно возвести в степень: ");
+double number = double.Parse(Console.ReadLine()!);
+Console.WriteLine();
+Console.Write("Введите степень, в которую нужно возвести число: ");
+double degree = double.Parse(Console.ReadLine()!);
+double degNum = DegreeOfNum(number, degree);
+Console.WriteLine();
+Console.WriteLine($"Число {number} в степени {degree} равно {degNum}");
+Console.WriteLine();
