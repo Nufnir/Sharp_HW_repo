b92070de6616430aecb585f9b0e67924c6bc2323
mikras <krasnov-mg@outlook.com>
2023-04-24 14:17:37 +0300

    Fifth seminar. Task 038. Make an array pf random numbers, caclulate a difference between max and min

---
+++ FifthSem_220423/Task038_diffMaxMinArray/Program.cs
@@ -0,0 +1,42 @@
+﻿void FillArray(double[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index < length)
+    {
+        collection[index] = new Random().NextDouble();
+        index++;
+    }
+}
+
+double MaxFinder(double[] collection)
+{
+    int count = collection.Length;
+    int position = 0;
+    double max = collection[position];
+    while(position < count)
+    {
+        if(collection[position] > max) max = collection[position];
+        position++;
+    }
+    return max;
+}
+
+double MinFinder(double[] collection)
+{
+    int count = collection.Length;
+    int position = 0;
+    double min = collection[position];
+    while(position < count)
+    {
+        if(collection[position] < min) min = collection[position];
+        position++;
+    }
+    return min;
+}
+
+double[] array = new double[10];
+FillArray(array);
+Console.WriteLine();
+Console.WriteLine($"Разность между максимальным и минимальным элементом массива равно {MaxFinder(array) - MinFinder(array)}");
+Console.WriteLine();
