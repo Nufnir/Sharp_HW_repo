d4188b720b25b149eeb34b2d5cb7d94741cc6994
mikras <krasnov-mg@outlook.com>
2023-05-21 16:57:48 +0300

    Seventh seminar. Task 052. Arithmetic mean of every column in 2d array

---
+++ SeventhSem_290423/Task052_ArithmeticMeanOfMArrayCol/Program.cs
@@ -0,0 +1,48 @@
+﻿void FillArray(int[,] matrix)
+{
+    for (int r = 0; r < matrix.GetLength(0); r++)
+    {
+        for (int c = 0; c < matrix.GetLength(1); c++)
+        {
+            matrix[r,c] = new Random().Next(1,9);
+        }
+    }
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int r = 0; r < matrix.GetLength(0); r++)
+    {
+        for (int c = 0; c < matrix.GetLength(1); c++)
+        {
+            Console.Write($"{matrix[r,c]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void PrintArrayColMean(int[,] matrix)
+{
+    for (int c = 0; c < matrix.GetLength(1); c++)
+    {
+        int sum = 0;
+        double avg = 0;
+        for (int r = 0; r < matrix.GetLength(0); r++)
+        {
+            sum += matrix[r,c];
+        }
+        avg = (double) sum / matrix.GetLength(0);
+        Console.WriteLine(avg);
+    }
+}
+
+
+int[,] mArray = new int[3, 4];
+
+FillArray(mArray);
+
+PrintArray(mArray);
+
+Console.WriteLine();
+
+PrintArrayColMean(mArray);
