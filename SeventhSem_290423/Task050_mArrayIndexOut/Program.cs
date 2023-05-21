76a8651c27afffad0f5ea9bf1ac5aeb6136bd549
mikras <krasnov-mg@outlook.com>
2023-05-21 16:56:18 +0300

    Seventh seminar. Task 050. Get a digit from a 2d array using input of index

---
+++ SeventhSem_290423/Task050_mArrayIndexOut/Program.cs
@@ -0,0 +1,42 @@
+﻿void FillArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = new Random().Next(1,9);
+        }
+    }
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int UserInput(string output)
+{
+    System.Console.Write(output);
+    string input = Console.ReadLine()!;
+    int result = Convert.ToInt32(input);
+    return result;
+}
+
+int[,] mArray = new int[3, 4];
+
+FillArray(mArray);
+
+int matrixRow = UserInput("Введите номер строки >> ");
+
+int matrixColumn = UserInput("Введите номер столбца >> ");
+
+PrintArray(mArray);
+
+Console.WriteLine(mArray[matrixRow, matrixColumn]);
