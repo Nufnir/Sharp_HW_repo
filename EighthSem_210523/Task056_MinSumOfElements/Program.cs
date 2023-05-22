66b23781cc5f9cd67b07663334d93c368818c9b9
mikras <krasnov-mg@outlook.com>
2023-05-22 12:20:33 +0300

    Eighth Seminar. Task 056. Get a line number of 2d array with minimal sum of elements

---
+++ EighthSem_210523/Task056_MinSumOfElements/Program.cs
@@ -0,0 +1,56 @@
+﻿void FillArray(int[,] matrix)
+{
+    for (int r = 0; r < matrix.GetLength(0); r++)
+    {
+        for (int c = 0; c < matrix.GetLength(1); c++)
+        {
+            matrix[r,c] = new Random().Next(1,9);
+        }
+    }
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int r = 0; r < matrix.GetLength(0); r++)
+    {
+        for (int c = 0; c < matrix.GetLength(1); c++)
+        {
+            Console.Write($"{matrix[r,c]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int SumLine(int[,] array, int r)
+{
+    int sum = array[r,0];
+    for (int c = 1; c < array.GetLength(1); c++)
+        {
+        sum += array[r,c];
+        }
+    return sum;
+}
+
+int[,] mArray = new int[3, 4];
+
+FillArray(mArray);
+
+PrintArray(mArray);
+
+Console.WriteLine();
+
+int minSum = 1;
+
+int sum = SumLine(mArray, 0);
+
+for (int i = 1; i < mArray.GetLength(0); i++)
+{
+    if (sum > SumLine(mArray, i))
+        {
+        sum = SumLine(mArray, i);
+        minSum = i+1;
+        }
+}
+
+Console.WriteLine($"Строка c наименьшей суммой элементов: {minSum}");
+
