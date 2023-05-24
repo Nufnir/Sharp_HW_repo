d8c295524385a76d6b6a83188aef1ad9be435d43
mikras <krasnov-mg@outlook.com>
2023-05-24 13:02:12 +0300

    Final Exam. Get two numbers from user. Compute Ackermann function (M, N).

---
+++ FinalExam/Task068_AfuncRec/Program.cs
@@ -0,0 +1,28 @@
+﻿int Depth = 0;
+
+UInt64 A(UInt64 m, UInt64 n)
+{
+    Depth++;
+    if (m == 0) return n + 1;
+    if (m > 0 && n == 0) return A(m - 1, 1);
+    return A(m - 1, A(m, n - 1));
+}
+
+
+Console.WriteLine();
+
+Console.Write($"Введите число M: ");
+
+UInt64 numberM = UInt64.Parse(Console.ReadLine()!);
+
+Console.WriteLine();
+
+Console.Write($"Введите число N: ");
+
+UInt64 numberN = UInt64.Parse(Console.ReadLine()!);
+
+Console.WriteLine();
+
+Console.WriteLine($"Функция Аккермана от чисел {numberM} и {numberN} равна {A(numberM, numberN)}");
+
+Console.WriteLine();
