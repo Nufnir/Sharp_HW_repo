f91efa7865ff70bf8a756e61b433414a99147898
mikras <krasnov-mg@outlook.com>
2023-05-24 12:59:12 +0300

    Final Exam. Task 066. Get two numbers from user. Sum all numbers between user input. Use recursion?

---
+++ FinalExam/Task066_SumFromMtoN/Program.cs
@@ -0,0 +1,28 @@
+﻿int numberM = 0;
+int numberN = 0;
+
+
+int Sum(int n)
+{
+    if (n == numberM) return numberM;
+    else return n + Sum(n - 1);
+}
+
+
+Console.WriteLine();
+
+Console.Write($"Введите число M: ");
+
+numberM = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine();
+
+Console.Write($"Введите число N: ");
+
+numberN = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine();
+
+Console.WriteLine($"Сумма чисел от {numberM} до {numberN} равна {Sum(numberN)}");
+
+Console.WriteLine();
