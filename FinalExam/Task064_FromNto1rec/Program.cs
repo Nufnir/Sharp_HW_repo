6fe586c9af614f82af6eb1accb0d3e53b9a6aec6
mikras <krasnov-mg@outlook.com>
2023-05-24 12:57:05 +0300

    Final Exam. Task 064. Get a number from user, print all numbers from user input to 1. Use recursion.

---
+++ FinalExam/Task064_FromNto1rec/Program.cs
@@ -0,0 +1,22 @@
+﻿string Ret(int a, int b)
+{
+    if (a <= b) return Ret(a + 1, b) + $"{a} ";
+    else return String.Empty;
+}
+
+
+int numberM = 1;
+
+Console.WriteLine();
+
+Console.Write($"Введите число, от которого нужно будет считать до {numberM}: ");
+
+int numberN = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine();
+
+Console.Write("Считаем: ");
+
+Console.WriteLine(Ret(numberM, numberN));
+
+Console.WriteLine();
